7e6dffec254baf8aa11f0d7019b7d414599c57f3
valentinabarbetty <113564513+valentinabarbetty@users.noreply.github.com>
2023-09-13 22:24:24 -0500

    pascal

--- taller1-BarbettyGomezGomez.rkt
+++ taller1-BarbettyGomezGomez.rkt
@@ -11,7 +11,7 @@
 ;; L -> L' : Procedimiento que recibe una lista con pares x,y e invierte esos pares de forma que queden y,x
 ;;
 ;; <lista> := ()
-;;         := (<lista>)
+;;         := (<valor-scheme> <lista>)
 
 (define invert
   (lambda (L)
@@ -33,7 +33,7 @@
 ;; L -> L' : Procedimiento que recibe una lista y a cada elemento de esa lista le añade un nivel más de paréntesis
 ;;
 ;; <lista> := ()
-;;         := (<lista>)
+;;         := (<valor-scheme> <lista>)
 
 (define down
   (lambda (L)
@@ -92,6 +92,13 @@
     )
   )
 
+;; cart-prod-helper :
+;; Proposito:
+;; x X L -> L' : Procedimiento que recibe un elemento x y lo agrega a
+;; cada elemento de la lista. 
+;; <lista> := ()
+;;         := (<valor-de-scheme> <lista>)
+
 (define cart-prod-helper
   (lambda (x L)
     (cond [(null? L) '()]
@@ -104,4 +111,132 @@
 ;; pruebas
 (cartesian-product '(a b c) '(x y))
 (cartesian-product '(p q r) '(5 6 7))
+
+
+
+;; punto 17
+
+;; mult:
+;; Proposito:
+;; L1 x L2 -> L' : Procedimiento que recibe dos listas y
+;; multiplica elemento por elemento estas dos,
+;; devolviendo una nueva lista con los productos correspondientes.
+;;
+;; <lista> := ()
+;;         := (<lista> <lista>)
+
+
+(define mult
+  (lambda (l1 l2)
+    (if (null? l1)
+        '()
+        (cons (* (car l1) (car l2))
+              (mult (cdr l1) (cdr l2))))))
+
+
+;; mult:
+;; Proposito:
+;; L1 x L2 -> L' : Procedimiento que recibe dos listas y realiza la
+;; multiplicación escalar entre la lista1 que es la matriz, y la lista 2
+;; que es el vector
+;;
+;; <lista> := ()
+;;         := (<lista> <lista>)
+
+
+(define prod-scalar-matriz
+  (lambda (mat vec)
+    (if (null? mat)
+        '()
+        (cons (mult (car mat) vec)
+              (prod-scalar-matriz (cdr mat) vec)))))
+
+
+;; pruebas
+(prod-scalar-matriz '((1 1) (2 2)) '(2 3))
+(prod-scalar-matriz '((1 1) (2 2) (3 3)) '(2 3))
+
+
+;; PUNTO 18
+;; sum:
+;; Proposito:
+;; L1 x L2 -> L': Procedimiento que recibe dos listas y realiza la
+;; suma entre ellas
+;;
+;; <lista> := ()
+;;         := (<lista> <lista>)
+
+(define sum
+  (lambda (l1 l2)
+    (if (null? l1)
+        '()
+        (cons (+ (car l1) (car l2))
+             (sum (cdr l1) (cdr l2))
+             )))) 
+
+;; cero-izq:
+;; Proposito:
+;; L -> L': Procedimiento que recibe una lista
+;; y añade un cero en la primera posición de esta
+;;
+;; <lista> := ()
+;;         := (<valor-de-scheme> <lista>)
+
+
+(define (cero-izq lst)
+  (cons 0 lst))
+
+;; cero-der:
+;; Proposito:
+;; L -> L': Procedimiento que recibe una lista
+;; y añade un cero en la ultima posición de esta
+;;
+;; <lista> := ()
+;;         := (<valor-de-scheme> <lista>)
+
+
+(define (cero-der lst)
+  (append lst (list 0)))
+
+
+;; sum:
+;; Proposito:
+;; x -> L: Procedimiento que recibe un número y calcula los números pascal
+;; hasta la fila x
+;;
+;; <lista> := ()
+;;         := (<lista><lista>)
+
+
+(define pascal
+  (lambda (x)
+   
+    (define (inner-function y c)
+      (if (= c 0)
+          y
+          (inner-function (sum(cero-izq y)(cero-der y)) (- c 1))
+          ))
+
+    (inner-function '(1) (- x 1))))
+
+
+;; pruebas
+(pascal 5)
+(pascal 1)
+
+
+
+
+
+
+
+
+
+
+
+      
+    
+
+
+
 
